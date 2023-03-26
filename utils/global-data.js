fb4b9df225cd0a236d458fb53e880df65fc38b69
ibrahimy353 <ibrahimy353@gmail.com>
2023-03-26 15:02:13 +0300

    modify footer

--- utils/global-data.js
+++ utils/global-data.js
@@ -7,7 +7,7 @@ export const getGlobalData = () => {
     : 'Building Stuff For the Net.';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved by Ibrahim.';
+    : 'All rights reserved by Him Ibra.';
 
   return {
     name,

4262e0119c24a4ac394407a1c9a4e934b2ca9b8c
ibrahimy353 <ibrahimy353@gmail.com>
2023-03-27 06:31:05 +0300

    changes

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,15 @@
 import Link from 'next/link';
-
+import image from '../img/tekvibe.png';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-   <div className="w-x h-x md:w-x1 md:h-y1 lg:w-x2 lg:h-y2">
-    <Image
+   <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-">
+    <Img
         src={img.tekvibe.png}
-        width={x}
-        height={y}
+        width={11}
+        height={11}
    />
+         {/* <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" /> */}
 </div>
       <p className="text-4xl dark:text-white text-center ">
         <Link href="/">

f60f67ddbee1f17459fbffd21be3e81c2717da98
ibrahimy353 <ibrahimy353@gmail.com>
2023-04-02 21:40:09 +0300

    changes

--- components/Header.js
+++ components/Header.js
@@ -9,6 +9,7 @@ export default function Header({ name }) {
     background: "url('https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')",
     height: '16vh',
     width: '16vh',
+    mx-auto,
     
 }}/>
 

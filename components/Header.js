3f74d59569775df1271b3ac0ef40efc54f8fd884
ibrahimy353 <ibrahimy353@gmail.com>
2023-04-05 21:44:18 +0300

    modify the display

--- components/Header.js
+++ components/Header.js
@@ -3,15 +3,15 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
  
-         {/* <div className="w-16 h-16 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" /> */}
+         <div className="w-16 h-16 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
 
-         <div style={{
+         {/* <div style={{
     background: "url('https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')",
     height: '16vh',
     width: '16vh',
     marginLeft: 'auto',
     marginRight: 'auto',
-}}/>
+}}/> */}
 
       <p className="text-xl dark:text-white text-center mx-auto">
         <Link href="/">

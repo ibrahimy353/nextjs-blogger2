bd0201f7d7ab631dca61eda4c7970e666043916f
ibrahimy353 <ibrahimy353@gmail.com>
2023-04-08 23:23:18 +0300

    changes

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header({ name }) {
          {/* <div className="w-16 h-16 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" /> */}
 
          <div style={{
-    background: "url('https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')",
+    background: "url('https://unsplash.com/photos/EwaV3HjwmUo=rb-1.2.1&auto=format&fit=crop&w=500&q=60')",
     height: '16vh',
     width: '16vh',
     marginLeft: 'auto',
